e5fa6842378eeca1a2a7a2758fe55ad34c47750d
arishuy <huybui19898@gmail.com>
2022-11-28 16:53:34 +0700

    u

--- src/main/java/model/DAO/AccountDAO.java
+++ src/main/java/model/DAO/AccountDAO.java
@@ -1,30 +1,46 @@
 package model.DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import model.Bean.Account;
 
 public class AccountDAO {
-
-	public static Account getAccount(String id, String pass) {
-		//Ket noi CSLD roi truy van!!
-		//Hard Code
-		List<Account> l = new ArrayList<Account>();
-		Account a1 = new Account();
-		a1.setUsername("admin");
-		a1.setPassword("123456");
-		l.add(a1);
-		
-		Account a2 = new Account();
-		a2.setUsername("huy");
-		a2.setPassword("982002");
-		l.add(a2);
-		for (Account a : l) {
-			if (a.getUsername().equals(id) && a.getPassword().equals(pass))
-				return a;
+	public String connectionURL = "jdbc:mysql://127.0.0.1:3306/ltm";
+	
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(connectionURL, "root", "");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
+		return connection;
+	}
+	public Account getAccount(String id, String pass) {
+		//Ket noi CSLD roi truy van!!
+		try {
+			Connection connection = getConnection();
+			Statement stat = connection.createStatement();
+			String sql = "SELECT * FROM user WHERE username='" + id +"'";
+			ResultSet rs = stat.executeQuery(sql);
+			while (rs.next()) {
+				if (rs.getString("password").equals(pass))
+					return new Account(rs.getString("id"),rs.getString("username"),rs.getString("password"),rs.getString("role"),rs.getString("name"),rs.getString("email"),rs.getString("phone"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		return null;
 	}
 
